5a59ba1e034e0e9c1403940d0462880994dd7bd4
Jonathan Delgado hi@jon.soy <hi@jon.soy>
2016-06-09 12:53:10 -0400

    Integra version en clase Appbase #1

--- src/Appbase.js
+++ src/Appbase.js
@@ -1,10 +1,19 @@
+import SemVer from 'semver'
+import pkg from '../package.json'
 const appbaseSymbol = Symbol( "Appbase" );
+
+const versionAppbase = SemVer( pkg.version )
 
 export class Appbase {
 	transport() {}
+	get VERSION () {
+		return versionAppbase
+	}
 	dataBase() {}
 	session() {}
 }
+
+Appbase.VERSION = versionAppbase
 
 export { appbaseSymbol }
 
