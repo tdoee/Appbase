f7fb2cc30cb71061c6cea02375310d9c44ff29e9
Jonathan Delgado hi@jon.soy <hi@jon.soy>
2016-06-09 12:38:28 -0400

    Acutualiza pruebas segun casos

--- spec/Appbase.spec.js
+++ spec/Appbase.spec.js
@@ -1,21 +1,15 @@
-import { Appbase as AppbaseWithA } from '../src/Appbase'
+import { Appbase as AppbaseAsMember } from '../src/Appbase'
 import Appbase from '../src/Appbase'
 
 
 describe( `Carga 'Appbase'`, function() {
-
-	it( 'si esta definida la variable utilizando opción a', function() {
-
-		// Si  es definido Appbase
-		expect( AppbaseWithA ).toBeDefined();
-
-	} )
-
-	it( 'si esta definida la variable utilizando opción general', function() {
-
+	it( `\`import Appbase from 'Appbase'\` ...`, function() {
 		// Si  es definido Appbase
 		expect( Appbase ).toBeDefined();
-
 	} )
 
+	it( `\`import { Appbase } from 'Appbase'\` ...`, function() {
+		// Si  es definido Appbase
+		expect( AppbaseAsMember ).toBeDefined();
+	} )
 } )
