f1a4a8b1c47b43fbcf53d90b273db5348dc58b32
Jonathan Delgado hi@jon.soy <hi@jon.soy>
2016-06-13 19:11:36 -0400

    Update Syntax

--- src/drivers/TransportServerFetch.js
+++ src/drivers/TransportServerFetch.js
@@ -4,49 +4,47 @@ import express from 'express'
 
 export class TransportServerFetch extends TransportServer {
 
-  constructor(app) {
+  constructor( app ) {
     super()
     this.app = app
   }
 
-  use(fn){}
-
   setUp() {
     let router = this.generator()
-    // por cada request sin importar el tipo
-    router.use(function(req, res, next) {
+      // por cada request sin importar el tipo
+    router.use( function( req, res, next ) {
       // hacer algo
-      console.log("log tsf use")
+      console.log( "log tsf use" )
       next();
-    })
+    } )
 
     //   api/push
-    router.post('/push',(req, res, next) => {
-      console.log("log tsf push")
-    })
+    router.post( '/push', ( req, res, next ) => {
+      console.log( "log tsf push" )
+    } )
 
     //   api/update
-    router.post('/update',(req, res, next) => {
-      console.log("log tsf update")
-    })
+    router.post( '/update', ( req, res, next ) => {
+      console.log( "log tsf update" )
+    } )
 
     //   api/remove
-    router.post('/remove',(req, res, next) => {
-      console.log("log tsf remove")
-    })
+    router.post( '/remove', ( req, res, next ) => {
+      console.log( "log tsf remove" )
+    } )
 
     //   api/set
-    router.post('/set',(req, res, next) => {
-      console.log("log tsf set")
-    })
+    router.post( '/set', ( req, res, next ) => {
+      console.log( "log tsf set" )
+    } )
 
     //  api/request
-    router.post('/',(req, res, next) => {
-    console.log("log tsf request")
-    })
+    router.post( '/', ( req, res, next ) => {
+      console.log( "log tsf request" )
+    } )
 
     // los path comienzan con /api/
-    this.app.use('/api', router)
+    this.app.use( '/api', router )
   }
 
   generator() {
@@ -54,8 +52,8 @@ export class TransportServerFetch extends TransportServer {
     return route
   }
 
-  request(headers = {}, body = {}) {
-    super.request(headers, body)
+  request( headers = {}, body = {} ) {
+    super.request( headers, body )
   }
 
 }
