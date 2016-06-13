7c67c86361a6fcedf005163993e89f73ba856640
Jonathan Delgado hi@jon.soy <hi@jon.soy>
2016-06-13 19:28:30 -0400

    Order code

--- src/plugins/TransportFetch.js
+++ src/plugins/TransportFetch.js
@@ -4,110 +4,110 @@ import fetch from 'node-fetch'
 
 export class TransportFetch extends Transport {
 
-  constructor(app, url) {
-    super(app, url)
+  constructor( app, url ) {
+    super( app, url )
   }
 
-  request(data) {
-    return new Promise((resolve, reject) => {
-      fetch(this.url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'tokenId': 'testToken',
-          'sessionId': 'testSession'
-        },
-        body: JSON.stringify(data)
-      })
-      .then((res) => {
-        return res.json()
-      }).then((data) => {
-        resolve(data)
-      })
-      .catch((err) => {
-        reject(err)
-      })
-    });
+  request( data ) {
+    return new Promise( ( resolve, reject ) => {
+      fetch( this.url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'tokenId': 'testToken',
+            'sessionId': 'testSession'
+          },
+          body: JSON.stringify( data )
+        } )
+        .then( ( res ) => {
+          return res.json()
+        } ).then( ( data ) => {
+          resolve( data )
+        } )
+        .catch( ( err ) => {
+          reject( err )
+        } )
+    } );
   }
 
-  push(data) {
-    return new Promise((resolve, reject) => {
-      fetch(this.resolveUrl('push'), {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'tokenId': 'testToken',
-          'sessionId': 'testSession'
-        },
-        body: JSON.stringify(data)
-      })
-      .then(() => {
-        resolve()
-      })
-      .catch(err => {
-        reject(err)
-      })
-    });
+  push( data ) {
+    return new Promise( ( resolve, reject ) => {
+      fetch( this.resolveUrl( 'push' ), {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'tokenId': 'testToken',
+            'sessionId': 'testSession'
+          },
+          body: JSON.stringify( data )
+        } )
+        .then( () => {
+          resolve()
+        } )
+        .catch( err => {
+          reject( err )
+        } )
+    } );
   }
 
-  update(data) {
-    return new Promise((resolve, reject) => {
-      fetch(this.resolveUrl('update'), {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'tokenId': 'testToken',
-          'sessionId': 'testSession'
-        },
-        body: JSON.stringify(data)
-      })
-      .then(() => {
-        resolve()
-      })
-      .catch(err => {
-        reject(err)
-      })
-    });
+  update( data ) {
+    return new Promise( ( resolve, reject ) => {
+      fetch( this.resolveUrl( 'update' ), {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'tokenId': 'testToken',
+            'sessionId': 'testSession'
+          },
+          body: JSON.stringify( data )
+        } )
+        .then( () => {
+          resolve()
+        } )
+        .catch( err => {
+          reject( err )
+        } )
+    } );
   }
 
-  set(data) {
-    return new Promise((resolve, reject) => {
-      fetch(this.resolveUrl('set'), {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'tokenId': 'testToken',
-          'sessionId': 'testSession'
-        },
-        body: JSON.stringify(data)
-      })
-      .then(() => {
-        resolve()
-      })
-      .catch(err => {
-        reject(err)
-      })
-    });
+  set( data ) {
+    return new Promise( ( resolve, reject ) => {
+      fetch( this.resolveUrl( 'set' ), {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'tokenId': 'testToken',
+            'sessionId': 'testSession'
+          },
+          body: JSON.stringify( data )
+        } )
+        .then( () => {
+          resolve()
+        } )
+        .catch( err => {
+          reject( err )
+        } )
+    } );
   }
 
-  remove(data) {
-    return new Promise((resolve, reject) => {
-      fetch(this.resolveUrl('remove'), {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'tokenId': 'testToken',
-          'sessionId': 'testSession'
-        },
-        body: JSON.stringify(data)
-      })
-      .then(() => {
-        resolve()
-      })
-      .catch(err => {
-        reject(err)
-      })
-    });
+  remove( data ) {
+    return new Promise( ( resolve, reject ) => {
+      fetch( this.resolveUrl( 'remove' ), {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'tokenId': 'testToken',
+            'sessionId': 'testSession'
+          },
+          body: JSON.stringify( data )
+        } )
+        .then( () => {
+          resolve()
+        } )
+        .catch( err => {
+          reject( err )
+        } )
+    } );
   }
 }
 
