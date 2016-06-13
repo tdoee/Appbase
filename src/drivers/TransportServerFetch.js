62f62baeca7c73234ec53d435b1d144a39e53e00
Jonathan Delgado hi@jon.soy <hi@jon.soy>
2016-06-13 19:18:46 -0400

    Deprecated:  - Remove TransportServerFetch#generator()
    Added:
     - Add Documentation
     - Custon app define

--- src/drivers/TransportServerFetch.js
+++ src/drivers/TransportServerFetch.js
@@ -9,47 +9,46 @@ export class TransportServerFetch extends TransportServer {
     this.app = app
   }
 
-  setUp() {
-    let router = this.generator()
-      // por cada request sin importar el tipo
+  /**
+   * Inicializa las rutas en express
+   */
+  setUp( app = this.app ) {
+    let router = express.Router()
+
+    // por cada request sin importar el tipo
     router.use( function( req, res, next ) {
       // hacer algo
       console.log( "log tsf use" )
-      next();
+      next()
     } )
 
-    //   api/push
+    // api/push
     router.post( '/push', ( req, res, next ) => {
       console.log( "log tsf push" )
     } )
 
-    //   api/update
+    // api/update
     router.post( '/update', ( req, res, next ) => {
       console.log( "log tsf update" )
     } )
 
-    //   api/remove
+    // api/remove
     router.post( '/remove', ( req, res, next ) => {
       console.log( "log tsf remove" )
     } )
 
-    //   api/set
+    // api/set
     router.post( '/set', ( req, res, next ) => {
       console.log( "log tsf set" )
     } )
 
-    //  api/request
+    // api/request
     router.post( '/', ( req, res, next ) => {
       console.log( "log tsf request" )
     } )
 
     // los path comienzan con /api/
-    this.app.use( '/api', router )
-  }
-
-  generator() {
-    let route = express.Router()
-    return route
+    app.use( '/api', router )
   }
 
   request( headers = {}, body = {} ) {
